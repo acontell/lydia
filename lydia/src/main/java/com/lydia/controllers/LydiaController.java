e9e15f2ebaac8434c772dda20bdfceff40a3752a
acontell <arcadio.contell@avoristravel.com>
2020-10-10 11:31:10 +0200

    - Added Exception Handler and integration tests.

--- lydia/src/main/java/com/lydia/controllers/LydiaController.java
+++ lydia/src/main/java/com/lydia/controllers/LydiaController.java
@@ -1,14 +1,22 @@
 package com.lydia.controllers;
 
 import com.lydia.client.ApiClient;
+import com.lydia.client.exceptions.ApiCallRateLimitExceededException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.TOO_MANY_REQUESTS;
 
 @RestController
 @RequestMapping("/")
 public class LydiaController {
 
+    static final String RETRY_HEADER = "X-Rate-Limit-Retry-After-Seconds";
+    static final String DEFAULT_OFFSET_VALUE = "20";
     private final ApiClient client;
 
     public LydiaController(final ApiClient client) {
@@ -35,8 +43,17 @@ public class LydiaController {
     }
 
     @GetMapping(value = "/trades-history")
-    public Object getTradesHistory() {
+    public Object getTradesHistory(@RequestParam(defaultValue = DEFAULT_OFFSET_VALUE) int offset) {
 
-        return this.client.getTradesHistory(20);
+        return this.client.getTradesHistory(offset);
+    }
+
+    @ExceptionHandler(ApiCallRateLimitExceededException.class)
+    public ResponseEntity<Object> handleRateLimitExceededException(final ApiCallRateLimitExceededException e) {
+
+        return ResponseEntity
+                .status(TOO_MANY_REQUESTS)
+                .header(RETRY_HEADER, Long.toString(e.getTimeToWaitInSeconds()))
+                .build();
     }
 }
---
+++ lydia/src/test/java/com/lydia/controllers/LydiaControllerTest.java
@@ -0,0 +1,92 @@
+package com.lydia.controllers;
+
+import com.lydia.client.ApiClient;
+import com.lydia.client.exceptions.ApiCallRateLimitExceededException;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static com.lydia.controllers.LydiaController.DEFAULT_OFFSET_VALUE;
+import static com.lydia.controllers.LydiaController.RETRY_HEADER;
+import static java.lang.Integer.parseInt;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LydiaControllerTest {
+
+    private static final Object RESPONSE = "RESPONSE";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ApiClient apiClient;
+
+    @Test
+    void it_should_return_assets_info() {
+
+        given(this.apiClient.getAssetsInfo()).willReturn(RESPONSE);
+
+        this.assertRequestIsCorrect(get("/assets-info"));
+    }
+
+    @SneakyThrows
+    private void assertRequestIsCorrect(final MockHttpServletRequestBuilder requestBuilder) {
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("RESPONSE"));
+    }
+
+    @Test
+    void it_should_return_account_balance() {
+
+        given(this.apiClient.getAccountBalance()).willReturn(RESPONSE);
+
+        this.assertRequestIsCorrect(get("/account-balance"));
+    }
+
+    @Test
+    void it_should_return_trade_balance() {
+
+        given(this.apiClient.getTradeBalance("ZEUR")).willReturn(RESPONSE);
+
+        this.assertRequestIsCorrect(get("/trade-balance"));
+    }
+
+    @Test
+    void it_should_return_trades_history_with_default_offset_when_not_present() {
+
+        given(this.apiClient.getTradesHistory(parseInt(DEFAULT_OFFSET_VALUE))).willReturn(RESPONSE);
+
+        this.assertRequestIsCorrect(get("/trades-history"));
+    }
+
+    @Test
+    void it_should_return_trades_history_with_offset() {
+
+        given(this.apiClient.getTradesHistory(400)).willReturn(RESPONSE);
+
+        this.assertRequestIsCorrect(get("/trades-history").param("offset", "400"));
+    }
+
+    @Test
+    void it_should_return_too_many_requests() throws Exception {
+
+        given(this.apiClient.getTradesHistory(parseInt(DEFAULT_OFFSET_VALUE))).willThrow(new ApiCallRateLimitExceededException(100));
+
+        this.mockMvc.perform(get("/trades-history"))
+                .andExpect(status().isTooManyRequests())
+                .andExpect(header().string(RETRY_HEADER, "100"));
+    }
+}
