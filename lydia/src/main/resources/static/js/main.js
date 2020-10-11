ea3aeac80b8036d0f8872b5f2e248d6a6ceec4d4
acontell <arcadio.contell@avoristravel.com>
2020-10-11 09:37:05 +0200

    - Added devtools

--- lydia/src/main/resources/static/js/main.js
+++ lydia/src/main/resources/static/js/main.js
@@ -1,6 +1,22 @@
-var app = new Vue({
+new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Vue!'
+    data() {
+        return {
+            message: null,
+            loading: true,
+            errored: false
+        }
+    },
+    mounted() {
+        axios
+            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => {
+                this.message = response.data
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => this.loading = false)
     }
-});
+})
